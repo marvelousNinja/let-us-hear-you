a2fb113cd80aa03c55e1851f005f034c0614e923
Alexander Sologub <alexander.sologub@rightscale.com>
2016-05-08 18:08:09 +0300

    Add deploy tooling for external services

---
+++ create-external-services.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -e
+
+# TODO: Twilio user-provided?
+
+cf service analyst-cloudant || cf create-service cloudantNoSQLDB Shared analyst-cloudant
+cf service analyst-object-storage || cf create-service Object-Storage Free analyst-object-storage
+cf service analyst-speech-to-text || cf create-service speech_to_text standard analyst-speech-to-text
+cf service analyst-tone-analyzer || cf create-service tone_analyzer beta analyst-tone-analyzer
+
+cf service-keys analyst-cloudant | grep Credentials-1 || cf create-service-key analyst-cloudant Credentials-1
+cf service-keys analyst-object-storage | grep Credentials-1 || cf create-service-key analyst-object-storage Credentials-1
+cf service-keys analyst-speech-to-text | grep Credentials-1 || cf create-service-key analyst-speech-to-text Credentials-1
+cf service-keys analyst-tone-analyzer | grep Credentials-1 || cf create-service-key analyst-tone-analyzer Credentials-1
+
+cf service-key analyst-cloudant Credentials-1
+cf service-key analyst-object-storage Credentials-1
+cf service-key analyst-speech-to-text Credentials-1
+cf service-key analyst-tone-analyzer Credentials-1
