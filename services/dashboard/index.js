dc054f9a3c66d8730b913dd26918ebaafcd41330
Alexander Sologub <alexander.sologub@rightscale.com>
2016-05-10 14:31:36 +0300

    Add audio file uploads

--- services/dashboard/index.js
+++ services/dashboard/index.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
-var bodyParser = require('body-parser');
 var dotenv = require('dotenv');
 var Promise = require('bluebird');
 var Cloudant = require('cloudant');
+var multer = require('multer');
+var streamifier = require('streamifier');
+
+var upload = multer();
 
 dotenv.load();
 
@@ -12,13 +15,51 @@ var connection = Cloudant(process.env.CLOUDANT_URL);
 connection.db.create('let_us_hear_you');
 var database = Promise.promisifyAll(connection.db.use('let_us_hear_you'));
 
+// https://developer.ibm.com/recipes/tutorials/using-the-ibm-object-storage-for-bluemix-with-node-js-applications/
+// https://github.com/pkgcloud/pkgcloud/pull/461/files
+// https://github.com/pkgcloud/pkgcloud/issues/477
+// https://developer.ibm.com/answers/questions/269436/bluemix-object-storage-and-pkgcloud-130.html
+var pkgcloud = require('pkgcloud');
+var config = {
+  provider: 'openstack',
+  useServiceCatalog: true,
+  useInternal: false,
+  keystoneAuthVersion: 'v3',
+  authUrl: process.env.OBJECT_STORAGE_AUTH_URL,
+  tenantId: process.env.OBJECT_STORAGE_PROJECT_ID,
+  domainId: process.env.OBJECT_STORAGE_DOMAIN_ID,
+  username: process.env.OBJECT_STORAGE_USERNAME,
+  password: process.env.OBJECT_STORAGE_PASSWORD,
+  region: process.env.OBJECT_STORAGE_REGION
+};
+
+var storageClient = pkgcloud.storage.createClient(config);
+
+// TODO AS: Maybe there's a better way to log stuff?
+storageClient.on('log::*', function(message, object) {
+  if (object) {
+   console.log(this.event.split('::')[1] + ' ' + message);
+   console.dir(object);
+  }
+  else {
+    console.log(this.event.split('::')[1]  + ' ' + message);
+  }
+});
+
+storageClient.CONTAINER_META_PREFIX = '';
+
+// TODO AS: Callback is required... for some reason.
+storageClient.createContainer({
+  name: 'audio',
+  metadata: { 'X-Container-Read': '.r:*' },
+}, function() {});
+
 var app = express();
 app.set('view options', { layout: false });
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function(req, res) {
   database.list({ 'include_docs': true }, function(err, response) {
@@ -36,11 +77,41 @@ app.get('/', function(req, res) {
   });
 });
 
-app.post('/feedback', function(req, res) {
+app.post('/feedback', upload.single('audio'), function(req, res) {
   database.insertAsync({
     type: 'feedback',
     timestamp: +new Date()
   }).then(function(response) {
+    if (req.file) {
+      var uploadPromise = new Promise(function(resolve, reject) {
+        var fileStream = streamifier.createReadStream(req.file.buffer);
+
+        var writeStream = storageClient.upload({
+          container: 'audio',
+          remote: response.id + '/' + req.file.originalname
+        });
+
+        fileStream.pipe(writeStream);
+
+        writeStream.on('success', function(data) {
+          resolve(storageClient._serviceUrl + '/' + data.container + '/' + data.name);
+        });
+
+        writeStream.on('error', function(error) {
+          reject();
+        });
+      });
+
+      return uploadPromise.then(function(audioPath) {
+        return database.insertAsync({
+          type: 'audio_content',
+          path: audioPath,
+          feedback_id: response.id,
+          timestamp: +new Date()
+        });
+      });
+    }
+
     return database.insertAsync({
       type: 'text_content',
       content: req.body.content,
