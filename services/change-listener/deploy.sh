fa982d35b5f8af1ccf96143eca15c55c15ba9bde
Alexander Sologub <alexander.sologub@rightscale.com>
2016-05-14 13:41:22 +0300

    Change Cloudant feed settings

--- services/change-listener/deploy.sh
+++ services/change-listener/deploy.sh
@@ -6,8 +6,8 @@ wsk package bind /whisk.system/cloudant letUsHearYouDatabase \
   -p username $CLOUDANT_USERNAME \
   -p password $CLOUDANT_PASSWORD \
   -p host $CLOUDANT_HOST
+  -p dbname let_us_hear_you \
 
 wsk trigger create letUsHearYouDatabaseChanged \
   --feed letUsHearYouDatabase/changes \
-  --param dbname let_us_hear_you \
   --param includeDoc true
